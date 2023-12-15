ea9f7e10fe613f127beadce9242f34c6a6f31711
Rick1505 <Rickkleinjan.rk@gmail.com>
2023-12-15 20:25:45 +0100

    add a working contact form, only need to redirect user and remove input

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, TextAreaField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Email
 from datetime import date
 import smtplib
 import os
@@ -12,6 +12,13 @@ my_email = os.environ.get("EMAIL")
 my_password = os.environ.get("PASSWORD")
 
 app = Flask(__name__)
+
+class ContactForm(FlaskForm):
+    name = StringField(validators=[DataRequired()], render_kw={"placeholder": "Your Name"})
+    email = EmailField(validators=[DataRequired(), Email()], render_kw={"placeholder": "Your Email"})
+    subject = StringField(validators=[DataRequired()], render_kw={"placeholder": "Subject"})
+    message = TextAreaField(validators=[DataRequired()], render_kw={"placeholder": "Message"})
+    submit = SubmitField(label="Send Message")
 
 # SET UP A ENV SECRET KEY
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
@@ -29,9 +36,24 @@ def about():
 def blog():
     return render_template("blog.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+    
+    msg = f"Hello,\nThis mail is from {form.name.data} with the mailadress: {form.email.data}\n\n{form.message.data}"
+    
+    if form.validate_on_submit():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=my_password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=my_email,
+                msg=f"Subject:{form.subject.data} \n\n{msg}"
+            )
+        redirect(url_for("home"))
+    
+    return render_template("contact.html", form=form)
 
 @app.route("/feature")
 def feature():
