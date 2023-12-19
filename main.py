aadb9a02d7741d9ef882f15a38840fb5cde75dbb
Rick1505 <rickkleinjan.rk@gmail.com>
2023-12-19 11:50:52 +0100

    test

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def blog():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     form = ContactForm()
-    
+    msg2 = "TEST123"
     msg = f"Hello,\nThis mail is from {form.name.data} with the mailadress: {form.email.data}\n\n{form.message.data}"
     
     if form.validate_on_submit():
